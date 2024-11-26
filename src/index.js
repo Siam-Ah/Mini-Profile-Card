48b274bb008ac12fb3a23b0654ea94e4e4884b36
Siam <sahm0036@student.monash.edu>
2024-11-26 14:19:10 +1100

    mini profile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="haaland.jpg" alt="Erling Haaland" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Erling Haaland</h1>
+      <p>
+        Erling Haaland, born July 21, 2000, is a Norwegian striker for
+        Manchester City and Norway. Known for his speed, strength, and
+        finishing, he’s one of football’s brightest talents.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <p style={{ backgroundColor: "blue" }} className="skill">
+        Goal-Scoring
+      </p>
+      <p style={{ backgroundColor: "yellow" }} className="skill">
+        Strength
+      </p>
+      <p style={{ backgroundColor: "green" }} className="skill">
+        Speed
+      </p>
+      <p style={{ backgroundColor: "pink" }} className="skill">
+        Positioning
+      </p>
+      <p style={{ backgroundColor: "cyan" }} className="skill">
+        Work Ethic
+      </p>
+      <p style={{ backgroundColor: "orange" }} className="skill">
+        Team Play
+      </p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
